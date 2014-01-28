fdd959fe3490b0ad3b10e4615f1c31cc40fa1c76
CarrotsAreMediocre <Patrick.E.Shanahan@gmail.com>
2014-01-27 19:03:10 -0600

    Carte-blanche exception handling in do() method, as they can't be caught anyways.

--- plugin_manager.py
+++ plugin_manager.py
@@ -116,13 +116,16 @@ class PluginManager(object):
         """
         return_values = []
         for plugin in self.plugins:
-            if not plugin.active:
+            try:
+                if not plugin.active:
+                    continue
+                plugin.protocol = protocol
+                res = getattr(plugin, command, lambda _: True)(data)
+                if res is None:
+                    res = True
+                return_values.append(res)
+            except:
                 continue
-            plugin.protocol = protocol
-            res = getattr(plugin, command, lambda _: True)(data)
-            if res is None:
-                res = True
-            return_values.append(res)
         return all(return_values)
 
     def get_by_name(self, name):
