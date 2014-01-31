c56036002dd74a0bec9e12bc514e09f065c82f10
CarrotsAreMediocre <Patrick.E.Shanahan@gmail.com>
2014-01-31 17:27:20 -0600

    Added regex checking for player names on join.

--- core_plugins/player_manager/plugin.py
+++ core_plugins/player_manager/plugin.py
@@ -1,6 +1,9 @@
+import re
+
 from construct import Container
 from twisted.internet.task import LoopingCall
 from twisted.words.ewords import AlreadyLoggedIn
+
 from base_plugin import BasePlugin
 from manager import PlayerManager, Banned, Player
 from packets import client_connect, connect_response
@@ -16,6 +19,7 @@ class PlayerManagerPlugin(BasePlugin):
         self.player_manager = PlayerManager(self.config)
         self.l_call = LoopingCall(self.check_logged_in)
         self.l_call.start(.25)
+        self.regexes = self.config.plugin_config['name_removal_regexes']
 
     def check_logged_in(self):
         for player in self.player_manager.session.query(Player).filter_by(logged_in=True).all():
@@ -25,6 +29,14 @@ class PlayerManagerPlugin(BasePlugin):
     def on_client_connect(self, data):
         client_data = client_connect().parse(data.data)
         try:
+            original_name = client_data.name
+            for regex in self.regexes:
+                client_data.name = re.sub(regex, "", client_data.name)
+            if len(client_data.name.strip()) == 0:  # If the username is nothing but spaces.
+                raise NameError("Your name must not be empty!")
+            if client_data.name != original_name:
+                self.logger.info("Player tried to log in with name %s, replaced with %s.",
+                                 original_name, client_data.name)
             self.protocol.player = self.player_manager.fetch_or_create(
                 name=client_data.name,
                 uuid=str(client_data.uuid),
@@ -32,34 +44,28 @@ class PlayerManagerPlugin(BasePlugin):
                 protocol=self.protocol.id)
             return True
         except AlreadyLoggedIn:
-            dc_packet = build_packet(
-                packets.Packets.CONNECT_RESPONSE,
-                packets.connect_response().build(
-                    Container(
-                        success=False,
-                        client_id=0,
-                        reject_reason="You are already connected!"
-                    )
-                )
-            )
-            self.protocol.transport.write(dc_packet)
-            self.protocol.transport.loseConnection()
+            self.reject_with_reason("You're already logged in! If this is not the case, please wait 10 seconds and try again.")
             self.logger.info("Already logged in user tried to log in.")
         except Banned:
-            ban_packet = build_packet(
-                packets.Packets.CONNECT_RESPONSE,
-                packets.connect_response().build(
-                    Container(
-                        success=False,
-                        client_id=0,
-                        reject_reason="You have been banned!"
-                    )
-                )
-            )
-            self.protocol.transport.write(ban_packet)
-            self.protocol.transport.loseConnection()
+            self.reject_with_reason("You have been banned!")
             self.logger.info("Banned user tried to log in.")
             return False
+        except NameError as e:
+            self.reject_with_reason(str(e))
+
+    def reject_with_reason(self, reason):
+        rejection = build_packet(
+            packets.Packets.CONNECT_RESPONSE,
+            packets.connect_response().build(
+                Container(
+                    success=False,
+                    client_id=0,
+                    reject_reason=reason
+                )
+            )
+        )
+        self.protocol.transport.write(rejection)
+        self.protocol.transport.loseConnection()
 
     def after_connect_response(self, data):
         connection_parameters = connect_response().parse(data.data)
