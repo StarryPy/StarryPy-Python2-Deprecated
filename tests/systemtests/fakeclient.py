eb8fafb7a2e6ae5e7a608818e8152da95d76af79
Marv Cool <marv@hostin.is>
2014-02-01 22:42:51 +0100

    adds first actually running test

--- tests/systemtests/fakeclient.py
+++ tests/systemtests/fakeclient.py
@@ -1,18 +1,31 @@
+import twisted
 from twisted.test import proto_helpers
 from twisted.trial import unittest
-from config import ConfigurationManager
+from packets import Packets
+from utility_functions import build_packet
 from server import *
 import os
 
+class SBFake(object):
+    def __init__(self):
+        # Monkey patching the connectionMade method to not create a protocol to the gameserver
+        setattr(StarryPyServerProtocol,"connect_to_upstream",lambda x: None)
 
-class SBFakeClient(object):
+        # Monkey patching abortConnection onto our fake transports
+        setattr(proto_helpers.StringTransport,"abortConnection",lambda x: None)
+
+    def send_packet(self, packet_type, payload):
+        self.protocol.dataReceived(build_packet(packet_type, payload.replace(" ","").decode("hex")))
+
+class SBFakeClient(SBFake):
     def __init__(self,config_path):
+        super(SBFakeClient, self).__init__()
         global config
         global logger
         setup(config_path)
 
         # this creates a fake transport (think tcp connection) which has an API similiar to String
-        self.tr = proto_helpers.StringTransportWithDisconnection()
+        self.tr = proto_helpers.StringTransport()
         self.factory = StarryPyServerFactory()
         self.protocol = self.factory.buildProtocol("127.0.0.1")
         self.tr.protocol = self.protocol
@@ -20,15 +33,19 @@ class SBFakeClient(object):
         # Making a connection as a client to the proxy
         self.protocol.makeConnection(self.tr)
 
-    def send_string(self,packet_string):
-        self.protocol.dataReceived(packet_string.decode("hex"))
+    def make_sure_evething_is_stopped(self):
+        self.protocol.connectionLost()
+        for reactor_user in self.factory.registered_reactor_users:
+            if hasattr(reactor_user,"cancel"):
+                reactor_user.cancel()
+            else:
+                reactor_user.stop()
 
-class SBFakeServer(object):
+
+class SBFakeServer(SBFake):
     def __init__(self, client_side_protocol):
-        # Monkey patching the connectionMade method to not create a protocol to the gameserver
-        setattr(StarryPyServerProtocol,"connect_to_upstream",lambda x: None)
 
-        self.tr = proto_helpers.StringTransportWithDisconnection()
+        self.tr = proto_helpers.StringTransport()
         self.factory = StarboundClientFactory(client_side_protocol)
         self.protocol = self.factory.buildProtocol("127.0.0.1")
         self.tr.protocol = self.protocol
@@ -37,10 +54,6 @@ class SBFakeServer(object):
         # so we do this manually here:
         self.protocol.makeConnection(self.tr)
 
-
-    def send_string(self,packet_string):
-        self.protocol.dataReceived(packet_string.decode("hex"))
-
 class ConnectionTests(unittest.TestCase):
 
     def setUp(self):
@@ -48,5 +61,8 @@ class ConnectionTests(unittest.TestCase):
         self.server_side = SBFakeServer(self.client_side.protocol)
 
     def test_protocol_version(self):
-        self.server_side.send_string('00000274')
-        self.assertEqual(self.client_side.tr.value().encode("hex"), "00000274")
+        self.server_side.send_packet(Packets.PROTOCOL_VERSION,'00000274')
+        self.assertEqual(self.client_side.tr.value().encode("hex"), "00 08 00000274".replace(" ",""))
+
+    def tearDown(self):
+        self.client_side.make_sure_evething_is_stopped()
