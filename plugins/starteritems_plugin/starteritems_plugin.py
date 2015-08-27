c3091fd5f9becfff4bc31ff7509a9d49c2181081
Brzys <mr.brzys@gmail.com>
2015-08-27 18:42:03 +0200

    [PEP8] StarterItems

--- plugins/starteritems_plugin/starteritems_plugin.py
+++ plugins/starteritems_plugin/starteritems_plugin.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from base_plugin import SimpleCommandPlugin
 from utility_functions import give_item_to_player
 from plugins.core.player_manager_plugin import permissions, UserLevels
@@ -7,33 +8,43 @@ class StarterItems(SimpleCommandPlugin):
     """
     Welcomes new players by giving them a bunch of items.
     """
-    name = "starteritems_plugin"
-    depends = ["command_plugin", "player_manager_plugin"]
-    commands = ["starteritems"]
+    name = 'starteritems_plugin'
+    depends = ['command_plugin', 'player_manager_plugin']
+    commands = ['starteritems']
 
     def activate(self):
         super(StarterItems, self).activate()
-        self.player_manager = self.plugins['player_manager_plugin'].player_manager
+        self.player_manager = self.plugins[
+            'player_manager_plugin'
+        ].player_manager
 
     @permissions(UserLevels.GUEST)
     def starteritems(self, data):
-        """Gives you some starter items (only once).\nSyntax: /starteritems"""
+        """
+        Gives you some starter items (only once).
+        Syntax: /starteritems
+        """
         try:
             my_storage = self.protocol.player.storage
         except AttributeError:
             return
-        if not 'given_starter_items' in my_storage or my_storage['given_starter_items'] == "False":
-            my_storage['given_starter_items'] = "True"
+
+        if not my_storage.get('given_starter_items', False):
+            my_storage['given_starter_items'] = True
             self.give_items()
             self.send_greetings()
-            self.logger.info("Gave starter items to %s.", self.protocol.player.name)
+            self.logger.info(
+                'Gave starter items to %s.', self.protocol.player.name
+            )
             self.protocol.player.storage = my_storage
         else:
-            self.protocol.send_chat_message("^red;You have already received a starter pack :O")
+            self.protocol.send_chat_message(
+                '^red;You have already received a starter pack :O'
+            )
 
     def give_items(self):
-        for item in self.config.plugin_config["items"]:
-            given = give_item_to_player(self.protocol, item[0], item[1])
+        for item in self.config.plugin_config['items']:
+            give_item_to_player(self.protocol, item[0], item[1])
 
     def send_greetings(self):
-        self.protocol.send_chat_message(self.config.plugin_config["message"])
+        self.protocol.send_chat_message(self.config.plugin_config['message'])
